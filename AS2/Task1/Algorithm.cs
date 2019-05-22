3446fec0fa216fa50aa87f3b3310f8a004113dc6
Sefton Munro <30001654@student.toiohomai.ac.nz>
2019-05-22 16:22:10 +1200

    Task 1 complete

---
+++ AS2/Task1/Algorithm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Algorithm
+    {
+        private static int[] Numbers = new int[50];
+
+        private static void genArray()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = rand.Next(1, 100);
+            }
+        }
+
+        public static string displayArray()
+        {
+            genArray();
+            string display = "";
+            for(int i = 0; i < Numbers.Length; i++)
+            {
+                if((i % 10) == 0)
+                {
+                    display += "\n";
+                }
+                display += Numbers[i] + " ";
+            }
+            return display;
+        }
+
+        public static int[] getNumbers()
+        {
+            return Numbers;
+        }
+    }
+}
---
+++ AS2/Task2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Algorithm.displayArray());
+            int input = getInput();
+            Console.WriteLine($"The {input} max numbers are {findMaximum(Algorithm.getNumbers(), input)}");
+            Console.ReadLine();
+        }
+
+        public static int getInput()
+        {
+            int input1;
+            Console.WriteLine("Enter the number of maximum values you want to find.");
+            input1 = int.Parse(Console.ReadLine());
+            return input1;
+        }
+
+        public static string findMaximum(int[] Numbers, int input1)
+        {
+
+            ArrayList Numbers2 = new ArrayList();
+            Numbers2 = arrayList(Numbers, Numbers2);
+            string maxNums = "";
+            for (int i = 0; i < input1; i++)
+            {
+                int max = 0;
+                int index = 0;
+                for (int j = 0; j < Numbers2.Count; j++)
+                {
+                    if ((int)Numbers2[j] > max)
+                    {
+                        max = (int)Numbers2[j];
+                        index = j;
+                    }
+                }
+                maxNums += max + " ";
+                Numbers2.RemoveAt(index);
+            }
+            return maxNums;
+        }
+
+        public static ArrayList arrayList(int[] Numbers, ArrayList Numbers2)
+        {
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers2.Add(Numbers[i]);
+            }
+            return Numbers2;
+        }
+
+
+    }
+}
